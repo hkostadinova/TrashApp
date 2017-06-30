ef4e51537f26bdcdef32c04979070ea35fac3a3f
hkostadinova <hkostadinova@gmail.com>
2017-07-01 01:54:44 +0300

    Save, Update and List trashes in DB

---
+++ Trash.java
@@ -0,0 +1,16 @@
+package com.example.administrator.tabzzf;
+
+/**
+ * Created by Kostadinova on 27.6.2017 г..
+ */
+
+public class Trash {
+    public String TrashId;
+    public String TrashName;
+    public String TrashDescription;
+    public String TrashLatitude;
+    public String TrashLongitude;
+    public String TrashIsCleaned;
+    public String TrashSize;
+
+}
